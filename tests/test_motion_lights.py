8046ac10c5a4178636eac26ea04b602a16d2307d
Daniel Mason <danielmason@iinet.net.au>
2018-12-12 17:33:52 +0800

    43 passed

--- tests/test_motion_lights.py
+++ tests/test_motion_lights.py
@@ -12,9 +12,67 @@ DELAY = 120;
 def ml(given_that):
     ml = MotionLight(None, None, None, None, None, None, None, None)
     given_that.passed_arg('__name__').is_set_to('david')
-    given_that.passed_arg('delay').is_set_to(DELAY)
     given_that.time_is(0)
     return ml
+
+
+@pytest.mark.parametrize("entity,entity_value", [
+    ('entity', TEST_LIGHT),
+    ('entity_on', TEST_LIGHT)
+    # ('entities', [TEST_LIGHT, TEST_LIGHT])
+])    
+@pytest.mark.parametrize("state_entity_value", [
+    (TEST_SENSOR),
+    ( [TEST_SENSOR, TEST_SENSOR]),
+    (None)
+])    
+@pytest.mark.parametrize("delay", [
+    (DELAY),
+    ( 26)
+])    
+
+
+@pytest.mark.parametrize("sensor,sensor_value", [
+    ('sensor',TEST_SENSOR),
+    ( 'sensors',[TEST_SENSOR, TEST_SENSOR]),
+    (None, None)
+]) 
+def test_basic(given_that, ml, assert_that, time_travel,entity,entity_value,state_entity_value,sensor,sensor_value,delay):
+    given_that.passed_arg(entity).is_set_to(entity_value)
+    given_that.passed_arg('state_entites').is_set_to(state_entity_value)
+    given_that.passed_arg(sensor).is_set_to(sensor_value)
+    given_that.passed_arg('delay').is_set_to(delay)
+    given_that.passed_arg(sensor).is_set_to(sensor_value)
+    # given_that.passed_arg('brightness').is_set_to(brightness)
+
+    ml.initialize()
+    given_that.mock_functions_are_cleared()
+    given_that.state_of(TEST_LIGHT).is_set_to('off') 
+    given_that.state_of(TEST_SENSOR).is_set_to('off') 
+    
+    time_travel.assert_current_time(0).seconds();
+
+    # Initial Motion
+    motion(ml)
+    # assert_that('light/turn_on').was.called_with(entity_id=TEST_LIGHT)
+    assert_that(TEST_LIGHT).was.turned_on()
+
+
+    time_travel.fast_forward(delay/2).seconds()
+    assert_that('light.test_light').was_not.turned_off()
+
+    # New motion
+    motion(ml)
+    assert_that('light.test_light').was_not.turned_off()
+    time_travel.fast_forward(delay).seconds()
+
+    # given_that.state_of('light.test_light').is_set_to('on') 
+
+    # After DELAY seconds
+    time_travel.assert_current_time(delay+delay/2).seconds()
+
+    assert_that(TEST_LIGHT).was.turned_off() # light should be turned off
+
 
 
 @pytest.mark.parametrize("entity,entity_value", [
@@ -22,21 +80,14 @@ def ml(given_that):
     ('entity_on', TEST_LIGHT),
     ('entities', [TEST_LIGHT, TEST_LIGHT])
 ])    
-@pytest.mark.parametrize("state_entity_value", [
-    (TEST_SENSOR),
-    ( [TEST_SENSOR, TEST_SENSOR]),
-    (None)
-])    
-
-@pytest.mark.parametrize("sensor,sensor_value", [
-    ('sensor',TEST_SENSOR),
-    ( 'sensors',[TEST_SENSOR, TEST_SENSOR]),
-    (None, None)
+@pytest.mark.parametrize("stay", [
+    (True),
+    ( False)
 ]) 
-def test_basic(given_that, ml, assert_that, time_travel,entity,entity_value,state_entity_value,sensor,sensor_value):
+def test_other(given_that, ml, assert_that, time_travel,entity,entity_value,stay):
     given_that.passed_arg(entity).is_set_to(entity_value)
-    given_that.passed_arg('state_entites').is_set_to(state_entity_value)
-    given_that.passed_arg(sensor).is_set_to(sensor_value)
+    # given_that.passed_arg(sensor).is_set_to(sensor_value)
+    given_that.passed_arg('stay').is_set_to(stay)
     # given_that.passed_arg('brightness').is_set_to(brightness)
 
     ml.initialize()
@@ -64,9 +115,10 @@ def test_basic(given_that, ml, assert_that, time_travel,entity,entity_value,stat
 
     # After DELAY seconds
     time_travel.assert_current_time(DELAY+DELAY/2).seconds()
-
-    assert_that(TEST_LIGHT).was.turned_off() # light should be turned off
-
+    if stay:
+        assert_that(TEST_LIGHT).was_not.turned_off() # light should be turned off
+    else:
+        assert_that(TEST_LIGHT).was.turned_off() # light should be turned off
 
 
 
