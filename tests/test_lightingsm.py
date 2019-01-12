856a0a96ddae3e3af77fb0c091d75c677e5c8017
Daniel Mason <danielmason@iinet.net.au>
2019-01-12 14:49:48 +0800

    fix(tests)

--- tests/test_lightingsm.py
+++ tests/test_lightingsm.py
@@ -85,7 +85,7 @@ class TestLightingSM:
         self.hass.block_till_done()
         # how to trigger state change? send event?
         
-        assert self.hass.states.get('SENSOR_ENTITY').state == 'on'
+        assert self.hass.states.get(SENSOR_ENTITY).state == 'on'
         assert self.hass.states.get('lightingsm.test').state == STATE_ACTIVE
         assert light.is_on(CONTROL_ENTITY)
         future = datetime.now() + timedelta(seconds=3)
