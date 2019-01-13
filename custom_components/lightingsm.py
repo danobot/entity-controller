2a3c534c930dd6275c35ca9adefee3e4a1af1388
Gitlab <gitlab-ci@danielha.tk>
2019-01-13 02:40:15 +0000

    chore(release): 2.3.14

--- custom_components/lightingsm.py
+++ custom_components/lightingsm.py
@@ -1,7 +1,7 @@
 """
 Entity timer component for Home Assistant Component
 Maintainer:       Daniel Mason
-Version:          v2.3.13
+Version:          v2.3.14
 Documentation:    https://github.com/danobot/appdaemon-motion-lights
 
 """
@@ -33,7 +33,7 @@ DOMAIN = 'lightingsm'
 CONSTRAIN_START = 1
 CONSTRAIN_END = 2
 
-VERSION = '2.3.13'
+VERSION = '2.3.14'
 SENSOR_TYPE_DURATION = 'duration'
 SENSOR_TYPE_EVENT = 'event'
 MODE_DAY = 'day'
