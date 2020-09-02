fbd5cf00e4b61a1c725edce55dc9026c3b032629
Daniel Mason <danobot@users.noreply.github.com>
2020-09-02 17:34:03 +0800

    Update const.py

--- custom_components/entity_controller/const.py
+++ custom_components/entity_controller/const.py
@@ -21,8 +21,8 @@ DOMAIN = "entity_controller"
 
 # services
 SERVICE_CLEAR_BLOCK = "clear_block"
-SERVICE_SET_STAY_ON = "set_stay_on"
-SERVICE_SET_STAY_OFF = "set_stay_off"
+SERVICE_ENABLE_STAY_MODE = "enable_stay_mode"
+SERVICE_DISABLE_STAY_MODE = "disable_stay_mode"
 SERVICE_SET_NIGHT_MODE = "set_night_mode"
 
 #configuration
