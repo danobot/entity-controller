84c2bfc8b65553580be4d3c1758b87f14dd766f3
GitHub Action <action@github.com>
2021-06-11 14:30:42 +0000

    chore(release): 9.2.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,11 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+<a name="9.2.7"></a>
+## [9.2.7](https://github.com/danobot/entity-controller/compare/v9.2.6...v9.2.7) (2021-06-11)
+
+
+
 <a name="9.2.6"></a>
 ## [9.2.6](https://github.com/danobot/entity-controller/compare/v9.2.5...v9.2.6) (2021-06-11)
 
--- custom_components/entity_controller/__init__.py
+++ custom_components/entity_controller/__init__.py
@@ -18,7 +18,7 @@ along with Entity Controller.  If not, see <https://www.gnu.org/licenses/>.
 """
 Entity controller component for Home Assistant.
 Maintainer:       Daniel Mason
-Version:          v9.2.6
+Version:          v9.2.7
 Project Page:     https://danielbkr.net/projects/entity-controller/
 Documentation:    https://github.com/danobot/entity-controller
 """
@@ -105,7 +105,7 @@ from .entity_services import (
 
 
 
-VERSION = '9.2.6'
+VERSION = '9.2.7'
 
 
 _LOGGER = logging.getLogger(__name__)
